29414a6712029f8e5d0c4a73911734d92f775584
remiyeku <antakin4@gmail.com>
2022-11-10 16:57:34 +0000

    task 4 to 6 completed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main">
-    {samplePosts.map((post)=> {
-      return <Post key={post.postId} title={post.title} date={post.date} author={post.author} text={post.text} highlights={post.highlights} img={post.image}></Post>
-    })}
-  </main>;
+  return (
+    <main id="main">
+      {samplePosts.map((post) => {
+        return (
+          <Post
+            key={post.postId}
+            title={post.title}
+            date={post.date}
+            author={post.author}
+            text={post.text}
+            highlights={post.highlights}
+            src={post.image.link}
+            alt={post.image.alt}
+          ></Post>
+        );
+        {
+        }
+      })}
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post({postId, title, author, date, text, highlights, image}) {
-  return <article key={postId} title={title} date={date} author={author} text={text} highlights={highlights} img={image}></article>;
+function Post({ postId, title, author, date, text, highlights, alt, src }) {
+  return (
+    <article id="post">
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <p>{author}</p>
+      <p>{text}</p>
+      <ul>
+        {highlights.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
+      <img src={src} alt={alt}></img>
+    </article>
+  );
 }
 
 export default Post;
+//highlights={highlights} img={image}
